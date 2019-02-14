c5d794731d7c33212a3bf49d51d42cf678beb371
Capelli <the.1man@hotmail.com>
2019-02-14 12:26:16 +0100

    Add device distance helper functions

--- examples/CUDA/cuda_device_utility.cu
+++ examples/CUDA/cuda_device_utility.cu
@@ -5,16 +5,29 @@
 //DEVICE SYMBOL VARIABLES (const & global)
 __device__ __constant__ UnitInfoDevice* device_unit_lookup;
 
-__device__ float GetFloatMapPos(float* ptr, size_t pitch, int x, int y) {
+__device__ float GetMapValue(cudaPitchedPtr map, int x, int y) {
+	char* ptr = (char*)map.ptr;
+	size_t pitch = map.pitch;
+
 	return *((float*)((char*)ptr + y * pitch) + x);
 }
 
-__device__ void SetFloatMapPos(float* ptr, size_t pitch, int x, int y, float value) {
-	//float* pElement = (float*)((char*)ptr + y * pitch) + x;
-	//*pElement = value;
+__device__ void SetMapValue(cudaPitchedPtr map, int x, int y, float value) {
+	char* ptr = (char*)map.ptr;
+	size_t pitch = map.pitch;
 
 	float* row = (float*)((char*)ptr + y * pitch);
 	row[x] = value;
-		
-	printf("(<%d, %d>, %f)", x, y, value);
+}
+
+__device__ float FloatDistance(int posX1, int posY1, int posX2, int posY2) {
+	float a = powf(posX1 - posX2, 2);
+	float b = powf(posY1 - posY2, 2);
+	return rsqrtf(a + b);
+}
+
+__device__ int IntDistance(int posX1, int posY1, int posX2, int posY2) {
+	int a = fabsf(posX1 - posX2);
+	int b = fabsf(posY1 - posY2);
+	return a + b;
 }
