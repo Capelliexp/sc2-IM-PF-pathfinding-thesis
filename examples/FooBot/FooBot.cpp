a15912526fdff848218c83073f4e42969868d8e9
Albin Sahlbom <albinsahlbom@hotmail.com>
2019-02-20 15:57:07 +0100

    Added test 3 for empty50

--- examples/FooBot/FooBot.cpp
+++ examples/FooBot/FooBot.cpp
@@ -156,6 +156,24 @@ void FooBot::CommandsOnEmpty50() {
 		}
 		break;
 	case 3:
+		if (spawn_units)
+		{
+			Debug()->DebugEnemyControl();
+			SpawnUnits(sc2::UNIT_TYPEID::TERRAN_MARINE, 1, sc2::Point2D(5, 5));
+			SpawnUnits(sc2::UNIT_TYPEID::PROTOSS_ZEALOT, 1, sc2::Point2D(25, 25), 2);
+			spawn_units = false;
+		}
+		//If the marine have spawned we can be sure that the zealot have spawned. And we don't realy have any way to check if it did.
+		else if (CheckIfUnitsSpawned(1, { sc2::UNIT_TYPEID::TERRAN_MARINE }))
+		{
+			SetDestination(Observation()->GetUnits(sc2::Unit::Alliance::Self, sc2::IsUnit(sc2::UNIT_TYPEID::TERRAN_MARINE)), sc2::Point2D(45, 45), sc2::ABILITY_ID::ATTACK);
+		}
+		if (CheckIfUnitsSpawned(1, { sc2::UNIT_TYPEID::PROTOSS_ZEALOT }))
+		{
+			SetBehavior(Observation()->GetUnits(sc2::IsUnit(sc2::UNIT_TYPEID::PROTOSS_ZEALOT)), sc2::ABILITY_ID::HOLDPOSITION);
+			spawn_units = true;
+			command = 0;
+		}
 		break;
 	case 4:
 		break;
