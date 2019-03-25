cfd467ded049086baed66c98f3e535841c8d8dcc
Albin Sahlbom <albinsahlbom@hotmail.com>
2019-03-25 09:45:24 +0100

    commands for height updated

--- examples/FooBot/FooBot.cpp
+++ examples/FooBot/FooBot.cpp
@@ -82,6 +82,12 @@ void FooBot::OnUnitDestroyed(const sc2::Unit * unit) {
 			return;
 		}
 	}
+	for (int i = 0; i < enemy_units.size(); ++i) {
+		if (enemy_units[i].unit == unit) {
+			enemy_units.erase(enemy_units.begin() + i);
+			return;
+		}
+	}
 	//Remove structure if destroyed
 	//Remove structure from IMs
 }
@@ -572,54 +578,57 @@ void FooBot::CommandsOnEmpty200() {
 void FooBot::CommandsOnHeight() {
 	switch (command) {
 	case 1:
-		if (spawn_units) {
-			SpawnUnits(sc2::UNIT_TYPEID::TERRAN_MARINE, 1, sc2::Point2D(5));
-			spawn_units = false;
+		if (spawned_player_units == 0) {
+			spawned_player_units = 1;
+			SpawnUnits(sc2::UNIT_TYPEID::TERRAN_MARINE, spawned_player_units, sc2::Point2D(5));
 		}
-		else if (CheckIfUnitsSpawned(1, { sc2::UNIT_TYPEID::TERRAN_MARINE })) {
-			SetDestination(Observation()->GetUnits(sc2::Unit::Alliance::Self, sc2::IsUnit(sc2::UNIT_TYPEID::TERRAN_MARINE)), sc2::Point2D(50, 60), sc2::ABILITY_ID::MOVE);
-			spawn_units = true;
+		else if (player_units.size() == spawned_player_units) {
+			SetDestination(player_units, sc2::Point2D(50, 60), behaviors::PASSIVE, false);
+			spawned_player_units = 0;
 			command = 0;
 		}
 		break;
 	case 2:
-		if (spawn_units) {
-			SpawnUnits(sc2::UNIT_TYPEID::TERRAN_MARINE, 1, sc2::Point2D(5));
-			spawn_units = false;
+		if (spawned_player_units == 0) {
+			spawned_player_units = 1;
+			SpawnUnits(sc2::UNIT_TYPEID::TERRAN_MARINE, spawned_player_units, sc2::Point2D(5));
 		}
-		else if (CheckIfUnitsSpawned(1, { sc2::UNIT_TYPEID::TERRAN_MARINE })) {
-			SetDestination(Observation()->GetUnits(sc2::Unit::Alliance::Self, sc2::IsUnit(sc2::UNIT_TYPEID::TERRAN_MARINE)), sc2::Point2D(95), sc2::ABILITY_ID::MOVE);
-			spawn_units = true;
+		else if (player_units.size() == spawned_player_units) {
+			SetDestination(player_units, sc2::Point2D(95), behaviors::PASSIVE, false);
+			spawned_player_units = 0;
 			command = 0;
 		}
 		break;
 	case 3:
-		if (spawn_units) {
-			SpawnUnits(sc2::UNIT_TYPEID::TERRAN_MARINE, 1, sc2::Point2D(5));
+		if (spawned_player_units == 0 && spawned_enemy_units == 0) {
+			spawned_player_units = 1;
+			SpawnUnits(sc2::UNIT_TYPEID::TERRAN_MARINE, spawned_player_units, sc2::Point2D(5));
 			SpawnUnits(sc2::UNIT_TYPEID::TERRAN_MARINE, 1, sc2::Point2D(18, 40), 2);
 			SpawnUnits(sc2::UNIT_TYPEID::TERRAN_MARINE, 1, sc2::Point2D(28, 25), 2);
 			SpawnUnits(sc2::UNIT_TYPEID::TERRAN_MARINE, 1, sc2::Point2D(75, 16), 2);
 			SpawnUnits(sc2::UNIT_TYPEID::TERRAN_MARINE, 1, sc2::Point2D(75, 50), 2);
 			SpawnUnits(sc2::UNIT_TYPEID::TERRAN_MARINE, 1, sc2::Point2D(55, 60), 2);
 			SpawnUnits(sc2::UNIT_TYPEID::TERRAN_MARINE, 1, sc2::Point2D(20, 14), 2);
-			spawn_units = false;
+			spawned_player_units = 6;
 		}
-		else if (CheckIfUnitsSpawned(1, { sc2::UNIT_TYPEID::TERRAN_MARINE })) {
-			SetDestination(Observation()->GetUnits(sc2::Unit::Alliance::Self, sc2::IsUnit(sc2::UNIT_TYPEID::TERRAN_MARINE)), sc2::Point2D(95), sc2::ABILITY_ID::MOVE);
+		else if (player_units.size() == spawned_player_units) {
+			SetDestination(player_units, sc2::Point2D(95), behaviors::DEFENCE, false);
 		}
-		if (CheckIfUnitsSpawned(1, { sc2::UNIT_TYPEID::TERRAN_MARINE })) {
-			SetBehavior(Observation()->GetUnits(sc2::Unit::Alliance::Enemy, sc2::IsUnit(sc2::UNIT_TYPEID::TERRAN_MARINE)), sc2::ABILITY_ID::HOLDPOSITION);
+		if (enemy_units.size() > 0) {
+			SetBehavior(enemy_units, sc2::ABILITY_ID::HOLDPOSITION);
 		}
-		if (Observation()->GetUnits(sc2::Unit::Alliance::Self, sc2::IsUnit(sc2::UNIT_TYPEID::TERRAN_MARINE)).size() == 1) {
-			if (sc2::Point2D(Observation()->GetUnits(sc2::Unit::Alliance::Self, sc2::IsUnit(sc2::UNIT_TYPEID::TERRAN_MARINE))[0]->pos) == sc2::Point2D(95)) {
-				spawn_units = true;
+		if (player_units.size() == spawned_player_units) {
+			if (sc2::Point2D(player_units[0].unit->pos) == sc2::Point2D(95)) {
+				spawned_player_units = 0;
+				spawned_enemy_units = 0;
 				command = 0;
 			}
 		}
 		break;
 	default:
+		spawned_player_units = 0;
+		spawned_enemy_units = 0;
 		command = 0;
-		spawn_units = true;
 		break;
 	}
 }
